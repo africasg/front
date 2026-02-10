b9fb15a2346f7486643656bf95a346333d5fe24d
africasg <afrimarsaga@gmail.com>
2026-02-10 19:08:13 +0100

    Limpito

--- src/components/header.tsx
+++
@@ -1,8 +0,0 @@
-export const nombrecillo = "Afri"
-export const nombrecillo2 = "Yolanda"
-const Header = ()=>{
-    return(
-        <h1>Este es el header de {nombrecillo}</h1>
-    )
-}
-export default Header;
--- src/components/parrafito.tsx
+++
@@ -1,9 +0,0 @@
-import { nombrecillo } from "./header"
-import { nombrecillo2 } from "./header"
-
-const Parrafillo = () =>{
-    return(
-        <p>Este es el parrafillo, en el header solo aparecía {nombrecillo} pero también está {nombrecillo2}</p>
-    )
-}
-export default Parrafillo;
